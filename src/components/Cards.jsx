beb183f44ee216c83903848a93f7758fb47e9f6f
MitaliShah <mitalishaleenshah@gmail.com>
2024-04-24 06:02:47 -0500

    I was stuck at condiitonally rendering background color based on title value, However I figured it out! :) const Wrapper = styled.div;

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,8 +1,6 @@
-// const generateCardBG = (title) => {
-//   const backgroundColor = hsl();
-//   if (title === "work") {
-//   }
-// };
+import workImg from "../../public/images/icon-study.svg";
+import styled from "styled-components";
+import playImg from "../../public/images/icon-play.svg";
 
 export default function Cards({ data, selectedView }) {
   return (
@@ -14,11 +12,34 @@ export default function Cards({ data, selectedView }) {
         console.log(`Current is :`, current, `Previous is`, previous);
 
         return (
-          <div key={title}>
-            <p>{title}</p>
-          </div>
+          <Wrapper key={title} title={title}>
+            <div>
+              <div>
+                <h2>{title}</h2>
+                <div>{/* <img src={workImg} alt="" /> */}</div>
+              </div>
+              <div></div>
+            </div>
+          </Wrapper>
         );
       })}
     </div>
   );
 }
+
+const Wrapper = styled.div`
+  background-color: ${(props) =>
+    props.title === "Work"
+      ? "var(--work-light-red)"
+      : props.title === "Play"
+      ? "var(--play-soft-blue)"
+      : props.title === "Study"
+      ? "var(--study-light-red)"
+      : props.title === "Exercise"
+      ? "var(--exercise-lime-green)"
+      : props.title === "Social"
+      ? "var(--social-violet)"
+      : props.title === "Self Care"
+      ? "var(--self-care-soft-orange)"
+      : null};
+`;
